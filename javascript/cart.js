5d94e3064d065146ea335923a2ed30f780676a57
Joaquín Correa <correaajm@gmail.com>
2022-08-23 00:18:55 -0300

    Operadores avanzados

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,20 +1,81 @@
-const cartStructure = document.getElementById("items")
+let cart=[];
 
+class CartElement {
+    constructor(product, amount) {
+        this.product = product;
+        this.amount = amount;
+    }
+}
+
+function storageCart(){
+    cart=JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+
+const cartStructure = document.getElementById("items")
+const footerCart = document.getElementById("footer")
+
+
+createCart() 
 
 function createCart() {
+
+    let totalCart =0;
+    cartStructure.innerHTML = "";
+
     cart.forEach(
         (structure) => {
-            let renglonCarrito = document.createElement("tr")
+            let cartLine = document.createElement("tr")
 
-            renglonCarrito.innerHTML = `
+            cartLine.innerHTML = `
+                <img src="../images/${structure.product.image}" alt="${structure.product.image}" class="card-img-top" style="width: 140px; height: 140px;">
                 <td>${structure.product.name}</td>
-                <td><input id="cantidad-producto-${elemento.producto.name}" type="number" value="${structure.amount}" min="1" max="100" step="1" style="width: 50px;"/></td>
-                <td>${structure.product.price}</td>
-                <td>${structure.product.price*structure.amount}</td>    
+                <td><input id="amount-product-${structure.product.name}" type="number" value="${structure.amount}" min="1" max="5" step="1" style="width: 50px;"/></td>
+                <td>$${structure.product.price}</td>
+                <td>$${structure.product.price*structure.amount}</td>    
             `;
 
-            cartStructure.append(renglonCarrito);
+            totalCart+=structure.product.price*structure.amount;
+
+            cartStructure.append(cartLine);
+
+            let inputAmountProduct = document.getElementById(`amount-product-${structure.product.name}`)
+
+            inputAmountProduct.addEventListener("change", (e) =>{
+                let newAmount = e.target.value;
+                structure.amount = newAmount;
+                createCart();
+            })
+
 
         }
-    )   
+    );
+
+ //inicia ternario   
+
+    cart. length == 0 ? footerCart.innerHTML = ` <th scope="row" colspan="5">Carrito vacío - no te olvides de comprar!!!</th> ` : footerCart.innerHTML = ` <th scope="row" colspan="5" style="font-size: 18px;">Total de la compra: $${(totalCart)}</th>`;    
+
+//finaliza ternario
+
+}       
+
+
+function emptyCart(){
+    document.getElementById("emptyCart").addEventListener("click", function(){
+        cart = []
+        localStorage.setItem("cart",JSON.stringify(cart));
+        alert("Vaciaste el carrito... Esperamos volver a verte!")
+        createCart()
+    });
 }
+
+emptyCart()
+
+
+//inicia el OR
+
+cart = JSON.parse(localStorage.getItem('cart')) || []
+createCart(cart)
+console.table(cart)
+
+//finaliza el or
--- javascript/juegos.js
+++ javascript/juegos.js
@@ -1,4 +1,3 @@
-let cart=[];
 const ps4 = []
 
 class Product {
@@ -8,18 +7,6 @@ class Product {
         this.price = parseInt(price);
       }
 }
-
-class CartElement {
-    constructor(product, amount) {
-        this.product = product;
-        this.amount = amount;
-    }
-}
-
-function storageCart(){
-    cart=JSON.parse(localStorage.getItem("cart")) || [];
-}
-
 
 ps4.push(new Product("Elden Ring","../images/EldenRing.jpg", 12000));
 ps4.push(new Product("Horizon Forbidden West", "../images/Horizon.jpg", 12000));
@@ -46,7 +33,11 @@ let GameCards=document.getElementById("PS4");
                 let cartElements = new CartElement(product, 1);
                 cart.push(cartElements);
                 localStorage.setItem("cart",JSON.stringify(cart));
-                alert("Agregaste "+games.name+" al Carrito.")
+                //inicia el desestructurador de objeto
+                let {name,price} = games
+                alert("Agregaste "+name+" al Carrito.")
+                console.table(name,"$"+price)
+                 //finaliza el desestructurador de objeto
                 createCart()
         }
         }
@@ -85,7 +76,11 @@ for(const games of ps5){
             let cartElements = new CartElement(product, 1);
             cart.push(cartElements);
             localStorage.setItem("cart",JSON.stringify(cart));
-            alert("Agregaste "+games.name+" al Carrito.")
+                //inicia el desestructurador de objeto
+                let {name,price} = games
+                alert("Agregaste "+name+" al Carrito.")
+                console.table(name,"$"+price)
+                 //finaliza el desestructurador de objeto
             createCart()
     }
     }
@@ -121,7 +116,11 @@ for(const games of SWITCH){
             let cartElements = new CartElement(product, 1);
             cart.push(cartElements);
             localStorage.setItem("cart",JSON.stringify(cart));
-            alert("Agregaste "+games.name+" al Carrito.")
+                //inicia el desestructurador de objeto
+                let {name,price} = games
+                alert("Agregaste "+name+" al Carrito.")
+                console.table(name,"$"+price)
+                 //finaliza el desestructurador de objeto
             createCart()
     }
     }
@@ -129,65 +128,3 @@ for(const games of SWITCH){
 }
 
 
-const cartStructure = document.getElementById("items")
-const footerCart = document.getElementById("footer")
-
-
-createCart() 
-
-function createCart() {
-
-    let totalCart =0;
-    cartStructure.innerHTML = "";
-
-    cart.forEach(
-        (structure) => {
-            let cartLine = document.createElement("tr")
-
-            cartLine.innerHTML = `
-                <img src="../images/${structure.product.image}" alt="${structure.product.image}" class="card-img-top" style="width: 140px; height: 140px;">
-                <td>${structure.product.name}</td>
-                <td><input id="amount-product-${structure.product.name}" type="number" value="${structure.amount}" min="1" max="5" step="1" style="width: 50px;"/></td>
-                <td>$${structure.product.price}</td>
-                <td>$${structure.product.price*structure.amount}</td>    
-            `;
-
-            totalCart+=structure.product.price*structure.amount;
-
-            cartStructure.append(cartLine);
-
-            let inputAmountProduct = document.getElementById(`amount-product-${structure.product.name}`)
-
-            inputAmountProduct.addEventListener("change", (e) =>{
-                let newAmount = e.target.value;
-                structure.amount = newAmount;
-                createCart();
-            })
-
-
-        }
-    );
-
-    if(cart.length == 0) {
-        footerCart.innerHTML = `
-            <th scope="row" colspan="5">Carrito vacío - no te olvides de comprar!!!</th>
-        `;
-    } else {
-        footerCart.innerHTML = `
-            <th scope="row" colspan="5" style="font-size: 18px;">Total de la compra: $${(totalCart)}</th>
-        `;
-    }
-
-}       
-
-
-function emptyCart(){
-    document.getElementById("emptyCart").addEventListener("click", function(){
-        cart = []
-        localStorage.setItem("cart",JSON.stringify(cart));
-        alert("Vaciaste el carrito... Esperamos volver a verte!")
-        createCart()
-    });
-}
-
-emptyCart()
